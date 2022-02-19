616e8afdf54c13e5c55597ed82f3c0bb57dcb0b4
Casper Andersson <casper@casan.se>
2022-02-19 11:37:53 +0100

    Finished publish script
    Signed-off-by: Casper Andersson <casper@casan.se>

--- publish.sh
+++ publish.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-ignore_list="README.md _config.yml Gemfile Gemfile.lock jekyll-clean-dark.gemspec publish.sh run.sh .git .gitignore .jekyll-cache"
+ignore_list="README.md _config.yml Gemfile Gemfile.lock jekyll-clean-dark.gemspec publish.sh run.sh"
 
 
 rm -r /var/www/casan.se/*
 cp -r _site/* /var/www/casan.se
+
+for file in $ignore_list
+do
+	rm "/var/www/casan.se/$file"
+done
+
