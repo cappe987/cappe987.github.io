960519995cba7a295e290f3434476d4a2557f703
Casper Andersson <casper.casan@gmail.com>
2022-08-25 09:23:39 +0200

    jekyll: move drafts to _drafts folder

--- scripts/docker-serve.sh
+++ scripts/docker-serve.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -p 4000:4000 -v $(pwd):/site bretfisher/jekyll-serve bundle exec jekyll serve --unpublished --host=0.0.0.0 $@
+docker run -p 4000:4000 -v $(pwd):/site bretfisher/jekyll-serve bundle exec jekyll serve --unpublished --drafts --host=0.0.0.0 $@
